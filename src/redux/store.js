906e951c7afe03c4be13f616308eda571a391506
Franco-Segesso <fransege@gmail.com>
2025-11-20 11:33:28 -0300

    creamos auth-slice y modificamos protectedroute y adminroute

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './slices/cartSlice';
 import productReducer from './slices/ProductSlice';
+import authReducer from './slices/AuthSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     products: productReducer,
     // Aquí agregaremos auth, products, etc. más adelante
+    auth: authReducer,
   },
 });
